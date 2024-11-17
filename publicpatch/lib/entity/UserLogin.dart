26c6c043bc7fb434536af31a3702092575d9fa27
RaulBus <raul.bus@student.upt.ro>
2024-11-17 10:47:42 +0200

    Add user entity and service for user management, including registration and login functionality

---
+++ publicpatch/lib/entity/UserLogin.dart
@@ -0,0 +1,14 @@
+class UserLogin {
+  String email;
+  String password;
+
+  UserLogin({required this.email, required this.password});
+
+  factory UserLogin.fromJson(Map<String, dynamic> json) {
+    return UserLogin(email: json['email'], password: json['password']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'email': email, 'password': password};
+  }
+}
